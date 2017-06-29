17d6ec69d815b9bbe03167ad2f7eb62c7bc48ee7
Zachary <zwjc1@students.waikato.ac.nz>
2017-06-29 16:22:29 +1200

    Update to support reactive counts when they are released - need to update README

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'aplc:grapher-react',
-    version: '0.0.2',
+    version: '0.0.3',
     summary: 'Extended React containers for Grapher queries.',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/zwjcarter/grapher-react',
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
         'underscore',
 
         'react-meteor-data@0.2.9',
-        'cultofcoders:grapher@1.2.4',
+        'cultofcoders:grapher@1.2.9',
     ]);
 
     api.mainModule('main.js');
